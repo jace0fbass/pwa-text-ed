203581334c6db207477948462272de9003eea030
Jason <jajones414@gmail.com>
2023-01-18 21:44:37 -0600

    Added logic for putDb and getDB to the database.js file.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+//open database
+const db = await openDB(dbName, 1) 
+//create transaction
+const tx = db.transaction(storeName, 'readwrite')
+//access store from transaction
+const store = tx.objectStore(storeName)
+//mutatate store
+await store.add()
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+//open database
+const db = await openDB(dbName, 1) 
+//create transaction
+const tx = db.transaction(storeName, 'readwrite')
+//access store from transaction
+const store = tx.objectStore(storeName)
+//mutate data
+return await store.getAll()
+}
 
 initdb();
