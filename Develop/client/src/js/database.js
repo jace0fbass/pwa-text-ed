f909380b6ec6972702a629e5daa771505371ccbf
Jason <jajones414@gmail.com>
2023-01-21 10:37:12 -0600

    Updated putDb and getDb in database.js file.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,39 +1,35 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-//open database
-const db = await openDB(dbName, 1) 
-//create transaction
-const tx = db.transaction(storeName, 'readwrite')
-//access store from transaction
-const store = tx.objectStore(storeName)
-//mutatate store
-await store.add()
-}
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log("Database updated!", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-//open database
-const db = await openDB(dbName, 1) 
-//create transaction
-const tx = db.transaction(storeName, 'readwrite')
-//access store from transaction
-const store = tx.objectStore(storeName)
-//mutate data
-return await store.getAll()
-}
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  return result;
+};
 
 initdb();
